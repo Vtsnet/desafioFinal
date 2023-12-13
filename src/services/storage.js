457b062e8e98d26047d165f160342b1cdbe0eb14
Valdir <vtsnet@gmail.com>
2023-12-13 00:59:52 -0300

    Teste

--- src/services/storage.js
+++ src/services/storage.js
@@ -1,42 +1,42 @@
-const aws = require("aws-sdk");
-require('aws-sdk/lib/maintenance_mode_message').suppress = true;
+// const aws = require("aws-sdk");
+// require('aws-sdk/lib/maintenance_mode_message').suppress = true;
 
-const endpoint = new aws.Endpoint(process.env.ENDPOINT_S3);
+// const endpoint = new aws.Endpoint(process.env.ENDPOINT_S3);
 
-const s3 = new aws.S3({
-    endpoint,
-    credentials: {
-        accessKeyId: process.env.KEY_ID,
-        secretAccessKey: process.env.APP_KEY
-    }
-});
+// const s3 = new aws.S3({
+//     endpoint,
+//     credentials: {
+//         accessKeyId: process.env.KEY_ID,
+//         secretAccessKey: process.env.APP_KEY
+//     }
+// });
 
-const uploadFile = async (path, buffer, mimetype) => {
+// const uploadFile = async (path, buffer, mimetype) => {
 
-    const file = await s3.upload({
-        Bucket: process.env.KEY_NAME,
-        Key: path,
-        Body: buffer,
-        ContentType: mimetype
+//     const file = await s3.upload({
+//         Bucket: process.env.KEY_NAME,
+//         Key: path,
+//         Body: buffer,
+//         ContentType: mimetype
 
-    }).promise();
+//     }).promise();
 
-    return {
-        url: file.Location,
-        path: file.Key
-    };
+//     return {
+//         url: file.Location,
+//         path: file.Key
+//     };
 
-};
+// };
 
-const deletArchives = async (path) => {
-    await s3.deleteObject({
-        Bucket: process.env.KEY_NAME,
-        Key: path
-    }).promise();
-};
+// const deletArchives = async (path) => {
+//     await s3.deleteObject({
+//         Bucket: process.env.KEY_NAME,
+//         Key: path
+//     }).promise();
+// };
 
 
-module.exports = {
-    uploadFile,
-    deletArchives,
-};
+// module.exports = {
+//     uploadFile,
+//     deletArchives,
+// };
